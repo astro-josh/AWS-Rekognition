ad21e7d7be0e0f2a9a6ffb45e82c522f327e85b1
obviousrebel <jalexa14@students.towson.edu>
2019-02-26 20:13:02 -0500

    Added user option, cleanup

--- test.py
+++ test.py
@@ -1,9 +1,6 @@
 import os
 import boto3
 from cv2 import *
-#from SimpleCV import Image, Camera
-#import pygame
-#import pygame.camera
 
 REGION = 'us-east-1'
 
@@ -17,9 +14,6 @@ FEATURES_BLACKLIST = ("Landmarks", "Emotions", "Pose", "Quality", "BoundingBox",
 
 
 def capture_image():
-    if os.path.exists(KEY):
-        os.remove(KEY)
-        print("Removed pic")
 
     # OpenCV (cv2)
     cam = VideoCapture(0)
@@ -34,57 +28,66 @@ def capture_image():
 
 
 def detect_labels(bucket, key, max_labels = 5, min_confidence = 95):
-	rekognition = boto3.client('rekognition', REGION)
-	response = rekognition.detect_labels(
-	    Image = {
-			'S3Object': {
-				'Bucket': bucket,
-				'Name': key,
-			}
-		},
-		MaxLabels = max_labels,
-		MinConfidence = min_confidence,
-	)
-	return response['Labels']
+    rekognition = boto3.client('rekognition', REGION)
+    response = rekognition.detect_labels(
+        Image = {
+            'S3Object': {
+                'Bucket': bucket,
+                'Name': key,
+            }
+        },
+        MaxLabels = max_labels,
+        MinConfidence = min_confidence,
+    )
+    
+    for label in response['Labels']:
+        print ("{Name} - {Confidence}".format(**label))
 
 
 def detect_faces(bucket, key, attributes = ['ALL']):
-	rekognition = boto3.client('rekognition', REGION)
-	response = rekognition.detect_faces(
-	    Image = {
-			'S3Object': {
-				'Bucket': bucket,
-				'Name': key,
-			}
-		},
-	    Attributes = attributes,
-	)
-	return response['FaceDetails']
+    rekognition = boto3.client('rekognition', REGION)
+    response = rekognition.detect_faces(
+        Image = {
+            'S3Object': {
+                'Bucket': bucket,
+                'Name': key,
+            }
+        },
+        Attributes = attributes,
+    )
+
+    for face in response['FaceDetails']:
+        print ("Face ({Confidence})".format(**face))
+        # emotions
+        for emotion in face['Emotions']:
+            print ("  {Type} : {Confidence}".format(**emotion))
+        # quality
+        for quality, value in face['Quality'].items():
+            print("  {quality} : {value}".format(quality=quality, value=value))
+        # facial features
+        for feature, data in face.items():
+            if feature not in FEATURES_BLACKLIST:
+                print ("  {feature} : {data}".format(feature=feature, data=data))
 
 
 def main():
-    capture_image()
+    opt = int(input("Type 0 for face detection, 1 for label detection, or -1 to exit: "))
 
-    s3 =  boto3.client('s3')
+    while opt != -1:
+        capture_image()
 
-    with open(KEY, "rb") as f:
-        s3.upload_fileobj(f, BUCKET, KEY)
+        s3 =  boto3.client('s3')
 
-    #for label in detect_labels(BUCKET, KEY):
-    #	print ("{Name} - {Confidence}".format(**label))
+        # upload to s3
+        with open(KEY, "rb") as f:
+            s3.upload_fileobj(f, BUCKET, KEY)
 
-    for face in detect_faces(BUCKET, KEY):
-    	print ("Face ({Confidence})".format(**face))
-    	# emotions
-    	for emotion in face['Emotions']:
-    		print ("  {Type} : {Confidence}".format(**emotion))
-    	# quality
-    	#for quality, value in face['Quality'].items():
-    	#	print("  {quality} : {value}".format(quality=quality, value=value))
-    	# facial features
-    	for feature, data in face.items():
-    		if feature not in FEATURES_BLACKLIST:
-    			print ("  {feature} : {data}".format(feature=feature, data=data))
+        if opt == 0:
+            detect_faces(BUCKET, KEY)
+        elif opt == 1:
+            detect_labels(BUCKET, KEY)
+
+        opt = int(input("Type 0 for face detection, 1 for label detection, or -1 to exit: "))
 
 
 if __name__ == "__main__":
